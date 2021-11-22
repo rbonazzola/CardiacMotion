dc3cc7330c075de198a1fee95c418280d4ba2656
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2021-11-22 17:26:27 +0000

    Minor style changes

--- models/model.py
+++ models/model.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn.functional as F
 from .layers import ChebConv_Coma, Pool
+from IPython import embed
+import numpy as np
+
 
 __author__ = ['Priyanka Patel', 'Rodrigo Bonazzola']
 
@@ -17,8 +20,8 @@ class Coma(torch.nn.Module):
           upsample_matrices, 
           adjacency_matrices, 
           n_nodes,
-          mode="testing"
-          z_aggr_function=lambda x: torch.Tensor.mean(x, axis=0)
+          mode="testing",
+          z_aggr_function=lambda x: torch.Tensor.mean(x, axis=1)
         ):
         
         super(Coma, self).__init__()
@@ -39,12 +42,11 @@ class Coma(torch.nn.Module):
         self._is_variational = is_variational
         
         self.A_edge_index, self.A_norm = self._build_adj_matrix()
-        self.cheb_enc = self._build_encoder()
-        self.cheb_dec = self._build_decoder()                
+        self.cheb_enc, self.cheb_dec = self._build_encoder(), self._build_decoder()                
         self.pool = Pool()
         
-        self.enc_lin_mu = torch.nn.Linear(self._n_features_before_z, self.z)
-        # Fully connected layer connecting the last pooling layer and the latent space layer.
+        # Fully connected layers connecting the last pooling layer and the latent space layer.
+        self.enc_lin_mu = torch.nn.Linear(self._n_features_before_z, self.z)        
         if self._is_variational:            
             self.enc_lin_var = torch.nn.Linear(self._n_features_before_z, self.z)
 
@@ -86,8 +88,9 @@ class Coma(torch.nn.Module):
 
 
     def set_mode(self, mode):
-        '''        
-        mode: "training" or "testing"
+        '''
+        params:
+          mode: "training" or "testing"
         '''
         self._mode = mode
 
@@ -107,6 +110,7 @@ class Coma(torch.nn.Module):
 
 
     def forward(self, x):
+
         batch_size = x.shape[0]
         time_frames = x.shape[1]
         
@@ -125,6 +129,11 @@ class Coma(torch.nn.Module):
 
 
     def encoder(self, x):
+
+        '''
+
+        '''
+
         for i in range(self.n_layers):
             x = self.cheb_enc[i](x, self.A_edge_index[i], self.A_norm[i])
             x = F.relu(x)
@@ -140,19 +149,28 @@ class Coma(torch.nn.Module):
                 log_var.append(self.enc_lin_var(x[:,i,:]))
         
         mu = torch.cat(mu)
+        mu = mu.reshape(-1, time_frames, self.z)
+        log_var
         mu = self.z_aggr_function(mu)
 
         if self._is_variational and self._mode == "training":
             log_var = torch.cat(log_var)
+            log_var = log_var.reshape(-1, time_frames, self.z)
             log_var = self.z_aggr_function(log_var)
             return mu, log_var
         else:
             return mu
 
     def decoder(self, x):
-        x = F.relu(self.dec_lin(x))
-        x = x.reshape(x.shape[0], -1, self.filters[-1])
-        for i in range(self.n_layers):
+        
+        '''
+        The decoder applies a phase embedding on the latent vector
+        '''
+
+        x = F.relu(self.dec_lin(x))        
+        x = x.reshape(x.shape[0], cheb_dec[0].in_channels, -1)
+        
+        for i in range(self.n_layers-1):
             x = self.pool(x, self.upsample_matrices[-i-1])
             x = self.cheb_dec[i](x, self.A_edge_index[self.n_layers-i-1], self.A_norm[self.n_layers-i-1])
             x = F.relu(x)
@@ -160,11 +178,12 @@ class Coma(torch.nn.Module):
         return x
 
 
-
 class Coma4D(Coma):
 
-    def __init__(self, *args):
-        super(ComaContentStyle).__init__(*args)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.dec_lin = torch.nn.Linear(2*self.z, self.filters[-1]*self.upsample_matrices[-1].shape[1])
 
 
     def encoder(self, x):
@@ -174,8 +193,8 @@ class Coma4D(Coma):
             x = F.relu(x)
             x = self.pool(x, self.downsample_matrices[i])
 
-        timeframes = x.shape[1]
-        x = x.reshape(x.shape[0], timeframes, self._n_features_before_z)
+        self.n_timeframes = x.shape[1]
+        x = x.reshape(x.shape[0], self.n_timeframes, self._n_features_before_z)
         
         mu, log_var = [], []
         for i in range(x.shape[1]):
@@ -183,30 +202,93 @@ class Coma4D(Coma):
             if self._is_variational and self._mode == "training":
                 log_var.append(self.enc_lin_var(x[:,i,:]))
         
-        mu = torch.cat(mu)                            
+        mu = torch.cat(mu)
+        mu = mu.reshape(-1, self.n_timeframes, self.z)        
         mu = self.z_aggr_function(mu)
 
         if self._is_variational and self._mode == "training":
             log_var = torch.cat(log_var)
+            log_var = log_var.reshape(-1, self.n_timeframes, self.z)
             log_var = self.z_aggr_function(log_var)
             return mu, log_var
         else:
             return mu
 
 
-    def decoder(self, x):
+    def decoder(self, z):
+    
+        z_t = self.phase_embedding(z)
+        # z_t = self.phase_embedding(z)
+        s_out = []
 
-        x = F.relu(self.dec_lin(x))
-        x = x.reshape(x.shape[0], -1, self.filters[-1])
-        for i in range(self.n_layers):
-            x = self.pool(x, self.upsample_matrices[-i-1])
-            x = self.cheb_dec[i](x, self.A_edge_index[self.n_layers-i-1], self.A_norm[self.n_layers-i-1])
-            x = F.relu(x)
-        x = self.cheb_dec[-1](x, self.A_edge_index[-1], self.A_norm[-1])
-        return x
+        for t in range(self.n_timeframes):
+
+            x = self.dec_lin(z_t[:,t,:])
+            x = F.relu(x)        
+
+            # x = x.reshape(-1, self.cheb_dec[0].in_channels)
+            x = x.reshape(x.shape[0], -1, self.cheb_dec[0].in_channels)
+            for i in range(self.n_layers-1):
+                x = self.pool(x, self.upsample_matrices[-i-1])
+                x = self.cheb_dec[i](x, self.A_edge_index[self.n_layers-i-1], self.A_norm[self.n_layers-i-1])
+                x = F.relu(x)
+            x = self.pool(x, self.upsample_matrices[0])
+            x = self.cheb_dec[-1](x, self.A_edge_index[-1], self.A_norm[-1])
+            
+            #TODO: comment this
+            x = x.unsqueeze(1)
+            s_out.append(x)
+
+        s_out = torch.cat(s_out, dim=1)
+        return s_out
 
 
-    pass
+    def forward(self, x):
+
+        batch_size = x.shape[0]
+        time_frames = x.shape[1]
+                
+        x = x.reshape(batch_size, time_frames, -1, self.filters[0])
+        
+        if self._is_variational and self._mode == "training":            
+            self.mu, self.log_var = self.encoder(x)            
+            z = self._sample(self.mu, self.log_var)
+            if torch.isinf(z).any():
+                self.mu, self.log_var = self.mu/1000, self.log_var/1000
+                z = self._sample(self.mu, self.log_var)
+        else:
+            z = self.encoder(x)
+
+        x = self.decoder(z)
+
+        return (self.mu, self.log_var), x
 
 
-# class Coma4D(Coma):
+    def phase_embedding(self, z):
+
+        '''
+        params:
+          z: a batched vector (N x M)
+
+        returns:
+          a phase-aware vector (N x T x 2M)
+        '''
+
+        exp_it = []
+
+        for i in range(self.n_timeframes):
+            phase = 2*np.pi*i/self.n_timeframes
+            exp_it.append([np.sin(phase), np.cos(phase)])
+
+        exp_it = np.array(exp_it)
+        exp_it = np.expand_dims(exp_it, axis=(0,2))
+        exp_it = torch.Tensor(exp_it)
+        exp_it = exp_it.reshape(self.n_timeframes, 1, 2)
+
+        # trick to achieve the desired dimensions
+        z = z.reshape(-1, 1, self.z, 1)
+        z_t = torch.matmul(z,exp_it)
+
+        z_t = z_t.reshape(*z_t.shape[:2], -1)
+
+        return z_t
