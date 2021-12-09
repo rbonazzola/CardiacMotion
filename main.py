9e1369585c7a72a093a627ab67ac261465171fd3
rbonazzola <rodbonazzola@gmail.com>
2021-12-09 13:14:04 +0000

    Modified main function and config parameters' names

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import sys
+import sys, os
+import yaml
+import logging
+import pickle as pkl
 import utils.VTKHelpers
-
-# sys.path.append("utils/VTKHelpers/")
 
 from CardiacMesh import Cardiac3DMesh, Cardiac4DMesh, CardiacMeshPopulation
 from models import layers
 
-import yaml
 from pprint import pprint
 from argparse import Namespace
-import logging
 
 import numpy as np
 import torch
@@ -19,8 +18,6 @@ import pytorch_lightning as pl
 import ipywidgets as widgets
 from IPython.display import display, HTML
 
-import os
-import pickle as pkl
 from utils import mesh_operations
 from utils.helpers import *
 from models.model import Coma4D
@@ -32,6 +29,65 @@ from mlflow.tracking import MlflowClient
 from config.load_config import load_config
 
 from data.DataModules import CardiacMeshPopulationDataset, CardiacMeshPopulationDM
+
+
+def get_matrices(config, cached_file):
+
+    M, A, D, U = mesh_operations.generate_transform_matrices(
+        Cardiac3DMesh(config.network_architecture.template_mesh), 
+        config.network_architecture.pooling.parameters.downsampling_factors
+    )
+
+    A_t, D_t, U_t = ([scipy_to_torch_sparse(x) for x in X] for X in (A, D, U))
+    n_nodes = [len(M[i].v) for i in range(len(M))]
+
+
+    if config.network_architeture.cached_matrices is not None:
+      A_t, D_t, U_t, n_nodes = pkl.load(open(config.network_architeture.cached_matrices, "rb"))
+
+    return {
+        "downsample_matrices": D_t,
+        "upsample_matrices": U_t,
+        "adjacency_matrices": A_t,
+        "n_nodes": n_nodes
+    }
+
+def get_datamodule(config):
+    
+    with open(config.dataset.cached_file) as ff:
+        data = pkl.load(ff)
+   
+    #TODO: MERGE THESE TWO INTO ONE DATAMODULE CLASS 
+    if config.dataset.data_type.startswith("cardiac"):
+        DataModule = CardiacMeshPopulationDM
+    elif config.dataset.data_type.startswith("synthetic"):
+        DataModule = SyntheticMeshesDM
+      
+    dm = DataModule(cardiac_population=data, batch_size=config.optimizer.batch_size)
+    return dm
+
+def get_coma_args(config, matrices):
+    coma_args = {
+        "num_features": config.network_architecture.n_features,
+        "n_layers": len(
+            config.network_architecture.convolution.parameters.channels
+        ),  # REDUNDANT
+        "num_conv_filters": config.network_architecture.convolution.parameters.channels,
+        "polygon_order": config.network_architecture.convolution.parameters.polynomial_degree,
+        "latent_dim": config.network_architecture.latent_dim,
+        "is_variational": config.loss.regularization_loss.weight != 0,
+    }
+
+    coma_args.update(matrices)
+
+    #{
+    #    "downsample_matrices": D_t,
+    #    "upsample_matrices": U_t,
+    #    "adjacency_matrices": A_t,
+    #    "n_nodes": n_nodes,
+    #    "mode": "testing",
+    #}
+    return coma_args
 
 # optimizers_menu = {
 #   "adam": torch.optim.Adam(coma.parameters(), lr=lr, betas=(0.5,0.99), weight_decay=weight_decay),
@@ -57,39 +113,10 @@ def print_auto_logged_info(r):
 
 def main(config):
 
-    """
-    TEST
-    """
-
-    with open("data/cached/cardiac_population_200_meshes.pkl", "rb") as ff:
-        kk = pkl.load(ff)
-
-    dm = CardiacMeshPopulationDM(cardiac_population=kk, batch_size=8)
-    # dm = CardiacMeshPopulationDM(config.root_folder, batch_size=2)
-
-    A_t, D_t, U_t, n_nodes = pkl.load(open("data/cached/matrices.pkl", "rb"))
-
-    # init model
-
-    coma_args = {
-        "num_features": config.network_architecture.n_features,
-        "n_layers": len(
-            config.network_architecture.convolution.parameters.channels
-        ),  # REDUNDANT
-        "num_conv_filters": config.network_architecture.convolution.parameters.channels,
-        "polygon_order": config.network_architecture.convolution.parameters.polynomial_degree,
-        "latent_dim": config.network_architecture.latent_dim,
-        "is_variational": config.loss.regularization_loss.weight != 0,
-        "downsample_matrices": D_t,
-        "upsample_matrices": U_t,
-        "adjacency_matrices": A_t,
-        "n_nodes": n_nodes,
-        "mode": "testing",
-    }
-
-    if config.log_to_mlflow:
-        mlflow.pytorch.autolog()
-
+    # LOAD DATA
+    # INIT MODEL
+    matrices = get_matrices("data/cached/matrices.pkl")
+    coma_args = get_coma_args(config, matrices)
     coma4D = Coma4D(**coma_args)
     model = CoMA(coma4D, config)
 
@@ -97,6 +124,7 @@ def main(config):
     trainer = pl.Trainer()
 
     if config.log_to_mlflow:
+        mlflow.pytorch.autolog()
         with mlflow.start_run() as run:
             trainer.fit(model, datamodule=dm)
     else:
@@ -119,24 +147,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-c", "--conf", help="path of config file", default="config/config.yaml"
     )
-    # parser.add_argument('-od', '--output_dir', default=None, help='path where to store output')
-    # parser.add_argument('-id', '--data_dir', default=None, help='path where to fetch input data from')
-    # parser.add_argument('--preprocessed_data', default=None, type=str, help='Location of cached input data.')
-    # parser.add_argument('--partition', default=None, type=str, help='Cardiac chamber.')
-    # parser.add_argument('--procrustes_scaling', default=False, action="store_true", help="Whether to perform scaling transformation after Procrustes alignment (to make mean distance to origin equal to 1).")
-    # parser.add_argument('--phase', default=None, help="cardiac phase (1-50|ED|ES)")
-    # parser.add_argument('--z', default=None, type=int, help='Number of latent variables.')
-    # parser.add_argument('--optimizer', default=None, type=str, help='optimizer (adam or sgd).')
-    # parser.add_argument('--epoch', default=None, type=int, help='Maximum number of epochs.')
-    # parser.add_argument('--nTraining', default=None, type=int, help='Number of training samples.')
-    # parser.add_argument('--nVal', default=None, type=int, help='Number of validation samples.')
-    # parser.add_argument('--kld_weight', type=float, default=None, help='Weight of Kullback-Leibler divergence.')
-    # parser.add_argument('--learning_rate', type=float, default=None, help='Learning rate.')
-    # parser.add_argument('--seed', default=None, help="Seed for PyTorch's Random Number Generator.")
-    # parser.add_argument('--stop_if_not_learning', default=None, action="store_true", help='Stop training if losses do not change.')
-    # parser.add_argument('--save_all_models', default=False, action="store_true",
-    #                     help='Save all models instead of just the best one until the current epoch.')
-    # parser.add_argument('--test', default=False, action="store_true", help='Set this flag if you just want to test whether the code executes properly. ')
+    
     parser.add_argument(
         "--log_to_mlflow",
         default=False,
@@ -153,39 +164,10 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # if args.conf is None:
-    #     args.conf = os.path.join(os.path.dirname(__file__), 'default.cfg')
-    #     logger.error('configuration file not specified, trying to load '
-    #           'it from current directory', args.conf)
-
-    ################################################################################
     ### Load configuration
     if not os.path.exists(args.conf):
         logger.error("Config not found" + args.conf)
 
     config = load_config(args.conf)
-
     config.log_to_mlflow = args.log_to_mlflow
-
     main(config)
-
-    # if args.data_dir:
-    #     config['data_dir'] = args.data_dir
-
-    # if args.output_dir:
-    #     config['output_dir'] = args.output_dir
-
-    # if args.test:
-    #     # some small values so that the execution ends quickly
-    #     config['comments'] = "this is a test"
-    #     config['nTraining'] = 500
-    #     config['nVal'] = 80
-    #     config['epoch'] = 20
-    #     config['output_dir'] = "output/test_{TIMESTAMP}"
-    # config['test'] = args.test
-
-    # overwrite_config_items(config, args)
-
-    # if args.dry_run:
-    #   pprint(config)
-    #   exit()
