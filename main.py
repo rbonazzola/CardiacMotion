186b2df31e1f98d39710420633c9b2e9b1f93946
rbonazzola <rodbonazzola@gmail.com>
2022-05-10 10:10:18 +0100

    Relocated helper functions

--- main.py
+++ main.py
@@ -1,213 +1,21 @@
-import pickle as pkl
-
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping 
-from pytorch_lightning.callbacks import RichModelSummary
-
-from IPython import embed
-from utils import mesh_operations
-from utils.helpers import *
-from models.model import Coma4D
-from models.model_c_and_s import Coma4D_C_and_S
-from models.ComaLightningModule import CoMA
-from torchviz import make_dot
-
+from pytorch_lightning.loggers import MLFlowLogger
 import mlflow.pytorch
 from mlflow.tracking import MlflowClient
 
-from psbody.mesh import Mesh
-from config.load_config import load_yaml_config, to_dict
+from config.cli_args import CLI_args, overwrite_config_items
 
-from data.DataModules import CardiacMeshPopulationDM
-from data.SyntheticDataModules import SyntheticMeshesDM
-from pytorch_lightning.loggers import MLFlowLogger
+from utils.helpers import *
+from torchviz import make_dot
+
+from config.load_config import load_yaml_config, to_dict
 
 import argparse
 import pprint
 
-def get_coma_matrices(config, dm, cache=True, from_cached=True):
-
-    '''
-    :param config: configuration Namespace, with a list called "network_architecture.pooling.parameters.downsampling_factors" as attribute.
-    :param dm: a PyTorch Lightning datamodule, with attributes train_dataset.dataset.mesh_popu and train_dataset.dataset.mesh_popu.template
-    :param cache: if True, will cache the matrices in a pkl file, unless this file already exists.
-    :param from_cached: if True, will try to fetch the matrices from a previously cached pkl file.
-    :return: a dictionary with keys "downsample_matrices", "upsample_matrices", "adjacency_matrices" and "n_nodes",
-    where the first three elements are lists of matrices and the last is a list of integers.
-    '''
-
-    mesh_popu = dm.train_dataset.dataset.mesh_popu
-    matrices_hash = hash((mesh_popu._object_hash, tuple(config.network_architecture.pooling.parameters.downsampling_factors))) % 1000000
-    cached_file = f"data/cached/matrices/{matrices_hash}.pkl"
-    
-    if from_cached and os.path.exists(cached_file):
-          A_t, D_t, U_t, n_nodes = pkl.load(open(cached_file, "rb"))
-    else:
-          template_mesh = Mesh(mesh_popu.template.vertices, mesh_popu.template.faces)
-          M, A, D, U = mesh_operations.generate_transform_matrices(
-            template_mesh, config.network_architecture.pooling.parameters.downsampling_factors,
-          )
-          n_nodes = [len(M[i].v) for i in range(len(M))]
-          A_t, D_t, U_t = ([scipy_to_torch_sparse(x).float() for x in X] for X in (A, D, U))
-          if cache:
-            os.makedirs(os.path.dirname(cached_file), exist_ok=True)
-            with open(cached_file, "wb") as ff:
-              pkl.dump((A_t, D_t, U_t, n_nodes), ff)
-
-    return {
-        "downsample_matrices": D_t,
-        "upsample_matrices": U_t,
-        "adjacency_matrices": A_t,
-        "n_nodes": n_nodes,
-        "template": template_mesh
-    }
-
-
-def get_coma_args(config, dm):
-
-    net = config.network_architecture
-
-    convs = net.convolution
-    coma_args = {
-        "num_features": net.n_features,
-        "n_layers": len(convs.channels_enc),  # REDUNDANT
-        "num_conv_filters_enc": convs.channels_enc,
-        "num_conv_filters_dec_c": convs.channels_dec_c,
-        "num_conv_filters_dec_s": convs.channels_dec_s,
-        "polygon_order": convs.parameters.polynomial_degree,
-        "latent_dim_content": net.latent_dim_c,
-        "latent_dim_style": net.latent_dim_s,
-        "is_variational": config.loss.regularization.weight != 0,
-        "mode": "testing",
-        "n_timeframes": config.dataset.parameters.T, 
-        "phase_input": net.phase_input,
-        "z_aggr_function": net.z_aggr_function
-    }
-
-    matrices = get_coma_matrices(config, dm, from_cached=False)
-    coma_args.update(matrices)
-    return coma_args
-
-
-def get_datamodule(config):
-
-    '''
-
-    '''
-
-    # TODO: MERGE THESE TWO INTO ONE DATAMODULE CLASS
-    if config.dataset.data_type.startswith("cardiac"):
-        return CardiacMeshPopulationDM(cardiac_population=data, batch_size=config.batch_size)
-    elif config.dataset.data_type.startswith("synthetic"):
-        return SyntheticMeshesDM(
-            batch_size=config.batch_size, 
-            data_params=config.dataset.parameters.__dict__, 
-            preprocessing_params=config.dataset.preprocessing
-        )
-
-
-def print_auto_logged_info(r):
-
-    tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
-    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
-    print("run_id: {}".format(r.info.run_id))
-    print("artifacts: {}".format(artifacts))
-    print("params: {}".format(r.data.params))
-    print("metrics: {}".format(r.data.metrics))
-    print("tags: {}".format(tags))
-
-
-def get_dm_model_trainer(config, trainer_args):
-
-    '''
-    Returns a tuple of (PytorchLightning datamodule, PytorchLightning model, PytorchLightning trainer)
-    '''
-
-    # LOAD DATA
-    dm = get_datamodule(config)
-    dm.setup()
-
-    # Initialize PyTorch model
-    coma_args = get_coma_args(config, dm)
-
-    coma4D = Coma4D_C_and_S(**coma_args)
-
-    # Initialize PyTorch Lightning module
-    model = CoMA(coma4D, config)
-
-    # trainer
-    trainer = pl.Trainer(
-        callbacks=[
-            EarlyStopping(monitor="val_loss", mode="min", patience=10),
-            RichModelSummary(max_depth=-1)            
-        ],
-        gpus=[trainer_args.gpus],
-        auto_select_gpus=trainer_args.auto_select_gpus,
-        min_epochs=trainer_args.min_epochs, max_epochs=trainer_args.max_epochs,
-        auto_scale_batch_size=trainer_args.auto_scale_batch_size,
-        logger=trainer_args.logger,
-        precision=trainer_args.precision,
-        overfit_batches=args.overfit_batches,
-    )
-
-    return dm, model, trainer
+from IPython import embed
 
 ###
-def get_mlflow_parameters(config):
-
-    loss = config.loss
-    net = config.network_architecture
-    loss_params = {
-        "w_kl": loss.regularization.weight,
-        "w_s": loss.reconstruction_s.weight
-    }
-    net_params = {
-        "latent_dim_s": net.latent_dim_s,
-        "latent_dim_c": net.latent_dim_c,
-        "z_aggr_function": net.z_aggr_function,
-        "convolution_type": net.convolution.type,
-        "n_channels_enc": net.convolution.channels_enc,
-        "n_channels_dec_c": net.convolution.channels_dec_c,
-        "n_channels_dec_s": net.convolution.channels_dec_s,
-        "reduction_factors": net.pooling.parameters.downsampling_factors,
-        "phase_input": net.phase_input
-    }
-
-    mlflow_parameters = {
-        "platform": check_output(["hostname"]).strip().decode(),
-        ** loss_params,
-        ** net_params,
-    }
-
-    return mlflow_parameters
-
-###
-def get_mlflow_dataset_params(config):
-
-    '''
-    Returns a dictionary containing the dataset parameters, to be logged to MLflow.
-    '''
-    d = config.dataset
-
-    mlflow_dataset_params = {
-         "dataset_type" : "synthetic",
-         "dataset_max_static_amplitude" : d.parameters.amplitude_static_max,
-         "dataset_max_dynamic_amplitude" : d.parameters.amplitude_dynamic_max,
-         "dataset_n_timeframes" : d.parameters.T,
-         "dataset_freq_max" : d.parameters.freq_max,
-         "dataset_l_max" : d.parameters.l_max,
-         "dataset_resolution": d.parameters.mesh_resolution,
-         "dataset_complexity_c": (d.parameters.l_max + 1) ** 2,
-         "dataset_complexity_s": ((d.parameters.l_max + 1) ** 2) * d.parameters.freq_max,
-         "dataset_complexity": ((d.parameters.l_max + 1) ** 2) * (d.parameters.freq_max + 1),
-         "dataset_random_seed" : d.parameters.random_seed,
-         "dataset_template": "icosphere", #TODO: add this as parameter in the configuration
-         "dataset_center_around_mean" : d.preprocessing.center_around_mean
-    }
-
-    return mlflow_dataset_params
-
-
 def main(config, trainer_args):
 
     #
@@ -263,14 +71,13 @@ def main(config, trainer_args):
         trainer.fit(model, datamodule=dm)
         result = trainer.test(datamodule=dm)
 
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description="Pytorch Trainer for Convolutional Mesh Autoencoders",
         argument_default=argparse.SUPPRESS
     )
-
-    from config.cli_args import CLI_args, ArgumentAction, overwrite_config_items
 
     #to avoid a little bit of boilerplate
     for k, v in CLI_args.items():
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,47 +1,18 @@
 import torch
 import sys; sys.path.append("..")
+
+from models.model_c_and_s import Coma4D_C_and_S
+from models.ComaLightningModule import CoMA
+
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import RichModelSummary
+
+from data.DataModules import CardiacMeshPopulationDM
+from data.SyntheticDataModules import SyntheticMeshesDM
+from utils import mesh_operations
 from VTKHelpers.CardiacMesh import Cardiac3DMesh as Mesh
-from torch.utils.data import TensorDataset, DataLoader, random_split
 from subprocess import check_output
-import os
 import shlex
-import pickle
-from .logger import logger
-import time
-
-def get_best_gpu_device():
-    '''
-    This function return the GPU with the greatest amount of free memory
-    '''
-    import nvgpu
-    gpu_info = nvgpu.gpu_info()
-    free_mem = [x['mem_total']-x['mem_used'] for x in gpu_info]
-    best_gpu_index = free_mem.index(max(free_mem))
-    best_gpu = int(gpu_info[best_gpu_index]['index'])
-    return best_gpu
-    
-    
-
-def get_device():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    if torch.cuda.is_available():
-      count = 1
-      while True:
-        try:
-          logger.error("Allocating GPU device, try %s..." % count)
-          best_gpu = get_best_gpu_device()
-          torch.cuda.set_device(best_gpu)
-          break
-        except:
-          count += 1
-          time.sleep(5)
-          if count == 10:
-            logger.error("Unable to allocate device %s after 10 tries. Aborting execution..." % best_gpu)
-            exit()
-          pass
-      logger.info('Choosing GPU number %s' % torch.cuda.current_device())
-    return device
-
 
 def scipy_to_torch_sparse(scp_matrix):
     import numpy as np
@@ -55,84 +26,184 @@ def scipy_to_torch_sparse(scp_matrix):
     return sparse_tensor
 
 
-def get_template_mesh(config):
-    from data.cardiac_mesh import CardiacMesh
-    template_file_path = config.get('template_fname', "template/template.vtk")
-    partition = config.get('partition', None)
-    subpart = CardiacMesh.SUBPARTS_IDS[partition] if partition is not None else None
-
-    template_mesh = Mesh(filename=template_file_path)
-    if subpart is not None:
-        template_mesh = template_mesh.extractSubpart(subpart)
-
-    return template_mesh
-
-
-def load_cardiac_dataset(config=None, mode="training"):
-    from data.cardiac_mesh import CardiacMesh
-
-    if config is None:
-        from config.config_parser import read_default_config
-        config = read_default_config()
-
-    if os.path.exists(config['preprocessed_data']):
-      logger.info("Loading pre-aligned mesh data from {}".format(config['preprocessed_data']))
-      dataset = pickle.load(open(config["preprocessed_data"], "rb"))
-      if mode == "training":
-          dataset.nTraining = config['nTraining']
-          dataset.nVal = config['nVal']
-          dataset.partition_dataset()
-    else:
-      logger.info("File {} not found. Creating dataset from scratch based on configuration.".format(config["preprocessed_data"]))
-      print("File {} not found. Creating dataset from scratch based on configuration.".format(config["preprocessed_data"]))
-      dataset = CardiacMesh(
-          nTraining=config['nTraining'],
-          nVal=config['nVal'],
-          meshes_file=config['data_dir'],
-          ids_file=config['ids_file'],
-          reference_mesh=get_template_mesh(config),
-          subpart=CardiacMesh.SUBPARTS_IDS[config['partition']],
-          procrustes_scaling=config["procrustes_scaling"],
-          procrustes_type=config["procrustes_type"],
-          mode=mode
-      )
-    
-    return dataset
-
-
-def get_cardiac_dataset_len(config):
-    import numpy as np
-    npy_file = config["data_dir"]
-    with open(npy_file, 'rb') as f:
-        major, minor = np.lib.format.read_magic(f)
-        shape, _, _ = np.lib.format.read_array_header_1_0(f)
-    return shape
-
 def get_current_commit_hash():
     return check_output(shlex.split("git rev-parse HEAD")).decode().strip()
 
 
-def get_loader(dataset, nTraining, nVal, batch_size, num_workers, shuffle=True):
+def get_datamodule(config):
+    '''
 
-    n_indiv = len(dataset.ids)
-    dataset = TensorDataset(torch.Tensor(dataset.point_clouds), torch.Tensor([int(x) for x in dataset.ids]))    
-    datasets = random_split(dataset, [nTraining, nVal, n_indiv - nTraining - nVal])
-    trainLoader =  DataLoader(dataset = datasets[0], batch_size = batch_size, shuffle=shuffle, num_workers=num_workers)
-    valLoader =  DataLoader(dataset = datasets[1], batch_size = 1, shuffle=shuffle, num_workers=num_workers)
-    testLoader =  DataLoader(dataset = datasets[2], batch_size = 1, shuffle=shuffle, num_workers=num_workers)
-    return trainLoader, valLoader, testLoader
+    '''
+
+    # TODO: MERGE THESE TWO INTO ONE DATAMODULE CLASS
+    if config.dataset.data_type.startswith("cardiac"):
+        return CardiacMeshPopulationDM(cardiac_population=data, batch_size=config.batch_size)
+    elif config.dataset.data_type.startswith("synthetic"):
+        return SyntheticMeshesDM(
+            batch_size=config.batch_size,
+            data_params=config.dataset.parameters.__dict__,
+            preprocessing_params=config.dataset.preprocessing
+        )
 
 
-# def get_loader(dataset, ids, batch_size, num_workers, shuffle=True):
-#     '''
-#     :param dataset:
-#     :param ids:
-#     :param batch_size:
-#     :param num_workers:
-#     :param shuffle:
-#     :return:
-#     '''
-#     ids = torch.Tensor([int(x) for x in ids])
-#     vertices = TensorDataset(torch.Tensor(dataset), ids)
-#     loader = DataLoader(vertices, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-#     return loader
+def get_dm_model_trainer(config, trainer_args):
+    '''
+    Returns a tuple of (PytorchLightning datamodule, PytorchLightning model, PytorchLightning trainer)
+    '''
+
+    # LOAD DATA
+    dm = get_datamodule(config)
+    dm.setup()
+
+    # Initialize PyTorch model
+    coma_args = get_coma_args(config, dm)
+
+    coma4D = Coma4D_C_and_S(**coma_args)
+
+    # Initialize PyTorch Lightning module
+    model = CoMA(coma4D, config)
+
+    # trainer
+    trainer = pl.Trainer(
+        callbacks=[
+            EarlyStopping(monitor="val_loss", mode="min", patience=10),
+            RichModelSummary(max_depth=-1)
+        ],
+        gpus=[trainer_args.gpus],
+        auto_select_gpus=trainer_args.auto_select_gpus,
+        min_epochs=trainer_args.min_epochs, max_epochs=trainer_args.max_epochs,
+        auto_scale_batch_size=trainer_args.auto_scale_batch_size,
+        logger=trainer_args.logger,
+        precision=trainer_args.precision,
+        overfit_batches=args.overfit_batches,
+    )
+
+    return dm, model, trainer
+
+
+def get_coma_matrices(config, dm, cache=True, from_cached=True):
+    '''
+    :param config: configuration Namespace, with a list called "network_architecture.pooling.parameters.downsampling_factors" as attribute.
+    :param dm: a PyTorch Lightning datamodule, with attributes train_dataset.dataset.mesh_popu and train_dataset.dataset.mesh_popu.template
+    :param cache: if True, will cache the matrices in a pkl file, unless this file already exists.
+    :param from_cached: if True, will try to fetch the matrices from a previously cached pkl file.
+    :return: a dictionary with keys "downsample_matrices", "upsample_matrices", "adjacency_matrices" and "n_nodes",
+    where the first three elements are lists of matrices and the last is a list of integers.
+    '''
+
+    mesh_popu = dm.train_dataset.dataset.mesh_popu
+    matrices_hash = hash(
+        (mesh_popu._object_hash, tuple(config.network_architecture.pooling.parameters.downsampling_factors))) % 1000000
+    cached_file = f"data/cached/matrices/{matrices_hash}.pkl"
+
+    if from_cached and os.path.exists(cached_file):
+        A_t, D_t, U_t, n_nodes = pkl.load(open(cached_file, "rb"))
+    else:
+        template_mesh = Mesh(mesh_popu.template.vertices, mesh_popu.template.faces)
+        M, A, D, U = mesh_operations.generate_transform_matrices(
+            template_mesh, config.network_architecture.pooling.parameters.downsampling_factors,
+        )
+        n_nodes = [len(M[i].v) for i in range(len(M))]
+        A_t, D_t, U_t = ([scipy_to_torch_sparse(x).float() for x in X] for X in (A, D, U))
+        if cache:
+            os.makedirs(os.path.dirname(cached_file), exist_ok=True)
+            with open(cached_file, "wb") as ff:
+                pkl.dump((A_t, D_t, U_t, n_nodes), ff)
+
+    return {
+        "downsample_matrices": D_t,
+        "upsample_matrices": U_t,
+        "adjacency_matrices": A_t,
+        "n_nodes": n_nodes,
+        "template": template_mesh
+    }
+
+
+def get_coma_args(config, dm):
+    net = config.network_architecture
+
+    convs = net.convolution
+    coma_args = {
+        "num_features": net.n_features,
+        "n_layers": len(convs.channels_enc),  # REDUNDANT
+        "num_conv_filters_enc": convs.channels_enc,
+        "num_conv_filters_dec_c": convs.channels_dec_c,
+        "num_conv_filters_dec_s": convs.channels_dec_s,
+        "polygon_order": convs.parameters.polynomial_degree,
+        "latent_dim_content": net.latent_dim_c,
+        "latent_dim_style": net.latent_dim_s,
+        "is_variational": config.loss.regularization.weight != 0,
+        "mode": "testing",
+        "n_timeframes": config.dataset.parameters.T,
+        "phase_input": net.phase_input,
+        "z_aggr_function": net.z_aggr_function
+    }
+
+    matrices = get_coma_matrices(config, dm, from_cached=False)
+    coma_args.update(matrices)
+    return coma_args
+
+
+def get_mlflow_parameters(config):
+    loss = config.loss
+    net = config.network_architecture
+    loss_params = {
+        "w_kl": loss.regularization.weight,
+        "w_s": loss.reconstruction_s.weight
+    }
+    net_params = {
+        "latent_dim_s": net.latent_dim_s,
+        "latent_dim_c": net.latent_dim_c,
+        "z_aggr_function": net.z_aggr_function,
+        "convolution_type": net.convolution.type,
+        "n_channels_enc": net.convolution.channels_enc,
+        "n_channels_dec_c": net.convolution.channels_dec_c,
+        "n_channels_dec_s": net.convolution.channels_dec_s,
+        "reduction_factors": net.pooling.parameters.downsampling_factors,
+        "phase_input": net.phase_input
+    }
+
+    mlflow_parameters = {
+        "platform": check_output(["hostname"]).strip().decode(),
+        **loss_params,
+        **net_params,
+    }
+
+    return mlflow_parameters
+
+
+###
+def get_mlflow_dataset_params(config):
+    '''
+    Returns a dictionary containing the dataset parameters, to be logged to MLflow.
+    '''
+    d = config.dataset
+
+    mlflow_dataset_params = {
+        "dataset_type": "synthetic",
+        "dataset_max_static_amplitude": d.parameters.amplitude_static_max,
+        "dataset_max_dynamic_amplitude": d.parameters.amplitude_dynamic_max,
+        "dataset_n_timeframes": d.parameters.T,
+        "dataset_freq_max": d.parameters.freq_max,
+        "dataset_l_max": d.parameters.l_max,
+        "dataset_resolution": d.parameters.mesh_resolution,
+        "dataset_complexity_c": (d.parameters.l_max + 1) ** 2,
+        "dataset_complexity_s": ((d.parameters.l_max + 1) ** 2) * d.parameters.freq_max,
+        "dataset_complexity": ((d.parameters.l_max + 1) ** 2) * (d.parameters.freq_max + 1),
+        "dataset_random_seed": d.parameters.random_seed,
+        "dataset_template": "icosphere",  # TODO: add this as parameter in the configuration
+        "dataset_center_around_mean": d.preprocessing.center_around_mean
+    }
+
+    return mlflow_dataset_params
+
+
+def print_auto_logged_info(r):
+
+    tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
+    print("run_id: {}".format(r.info.run_id))
+    print("artifacts: {}".format(artifacts))
+    print("params: {}".format(r.data.params))
+    print("metrics: {}".format(r.data.metrics))
+    print("tags: {}".format(tags))
