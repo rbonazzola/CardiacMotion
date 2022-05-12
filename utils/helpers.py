55081452cec68d87a2096f366b93c4de4fbf40fc
rbonazzola <rodbonazzola@gmail.com>
2022-05-12 18:20:29 +0100

    Split helper functions into specific modules

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,8 +1,6 @@
 import torch
+import os
 import sys; sys.path.append("..")
-
-from models.model_c_and_s import Coma4D_C_and_S
-from models.ComaLightningModule import CoMA
 
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks import RichModelSummary
@@ -10,11 +8,12 @@ from pytorch_lightning.callbacks import RichModelSummary
 from data.DataModules import CardiacMeshPopulationDM
 from data.SyntheticDataModules import SyntheticMeshesDM
 from utils import mesh_operations
-from VTKHelpers.CardiacMesh import Cardiac3DMesh as Mesh
-from subprocess import check_output
-import shlex
+from utils.mesh_operations import Mesh
+
+import pickle as pkl
 
 def scipy_to_torch_sparse(scp_matrix):
+
     import numpy as np
     indices = np.vstack((scp_matrix.row, scp_matrix.col))
     i = torch.LongTensor(indices)
@@ -26,59 +25,25 @@ def scipy_to_torch_sparse(scp_matrix):
     return sparse_tensor
 
 
-def get_current_commit_hash():
-    return check_output(shlex.split("git rev-parse HEAD")).decode().strip()
-
-
-def get_datamodule(config):
+def get_datamodule(config, perform_setup=True):
     '''
 
     '''
 
     # TODO: MERGE THESE TWO INTO ONE DATAMODULE CLASS
     if config.dataset.data_type.startswith("cardiac"):
-        return CardiacMeshPopulationDM(cardiac_population=data, batch_size=config.batch_size)
+        dm = CardiacMeshPopulationDM(cardiac_population=data, batch_size=config.batch_size)
     elif config.dataset.data_type.startswith("synthetic"):
-        return SyntheticMeshesDM(
+        dm = SyntheticMeshesDM(
             batch_size=config.batch_size,
             data_params=config.dataset.parameters.__dict__,
             preprocessing_params=config.dataset.preprocessing
         )
 
+    if perform_setup:
+        dm.setup()
 
-def get_dm_model_trainer(config, trainer_args):
-    '''
-    Returns a tuple of (PytorchLightning datamodule, PytorchLightning model, PytorchLightning trainer)
-    '''
-
-    # LOAD DATA
-    dm = get_datamodule(config)
-    dm.setup()
-
-    # Initialize PyTorch model
-    coma_args = get_coma_args(config, dm)
-
-    coma4D = Coma4D_C_and_S(**coma_args)
-
-    # Initialize PyTorch Lightning module
-    model = CoMA(coma4D, config)
-
-    # trainer
-    trainer = pl.Trainer(
-        callbacks=[
-            EarlyStopping(monitor="val_loss", mode="min", patience=10),
-            RichModelSummary(max_depth=-1)
-        ],
-        gpus=[trainer_args.gpus],
-        auto_select_gpus=trainer_args.auto_select_gpus,
-        min_epochs=trainer_args.min_epochs, max_epochs=trainer_args.max_epochs,
-        auto_scale_batch_size=trainer_args.auto_scale_batch_size,
-        logger=trainer_args.logger,
-        precision=trainer_args.precision,
-        overfit_batches=args.overfit_batches,
-    )
-
-    return dm, model, trainer
+    return dm
 
 
 def get_coma_matrices(config, dm, cache=True, from_cached=True):
@@ -144,66 +109,48 @@ def get_coma_args(config, dm):
     return coma_args
 
 
-def get_mlflow_parameters(config):
-    loss = config.loss
-    net = config.network_architecture
-    loss_params = {
-        "w_kl": loss.regularization.weight,
-        "w_s": loss.reconstruction_s.weight
-    }
-    net_params = {
-        "latent_dim_s": net.latent_dim_s,
-        "latent_dim_c": net.latent_dim_c,
-        "z_aggr_function": net.z_aggr_function,
-        "convolution_type": net.convolution.type,
-        "n_channels_enc": net.convolution.channels_enc,
-        "n_channels_dec_c": net.convolution.channels_dec_c,
-        "n_channels_dec_s": net.convolution.channels_dec_s,
-        "reduction_factors": net.pooling.parameters.downsampling_factors,
-        "phase_input": net.phase_input
-    }
+def get_lightning_module(config, dm):
 
-    mlflow_parameters = {
-        "platform": check_output(["hostname"]).strip().decode(),
-        **loss_params,
-        **net_params,
-    }
+    from models.model_c_and_s import Coma4D_C_and_S
+    from models.ComaLightningModule import CoMA
 
-    return mlflow_parameters
+    # Initialize PyTorch model
+    coma_args = get_coma_args(config, dm)
+    coma4D = Coma4D_C_and_S(**coma_args)
+
+    # Initialize PyTorch Lightning module
+    model = CoMA(coma4D, config)
+    return model
 
 
-###
-def get_mlflow_dataset_params(config):
+def get_lightning_trainer(trainer_args):
+
+    # trainer
+    trainer = pl.Trainer(
+        callbacks=[
+            EarlyStopping(monitor="val_loss", mode="min", patience=10),
+            RichModelSummary(max_depth=-1)
+        ],
+        gpus=[trainer_args.gpus],
+        auto_select_gpus=trainer_args.auto_select_gpus,
+        min_epochs=trainer_args.min_epochs, max_epochs=trainer_args.max_epochs,
+        auto_scale_batch_size=trainer_args.auto_scale_batch_size,
+        logger=trainer_args.logger,
+        precision=trainer_args.precision,
+        overfit_batches=trainer_args.overfit_batches,
+    )
+
+    return trainer
+
+
+def get_dm_model_trainer(config, trainer_args):
     '''
-    Returns a dictionary containing the dataset parameters, to be logged to MLflow.
+    Returns a tuple of (PytorchLightning datamodule, PytorchLightning model, PytorchLightning trainer)
     '''
-    d = config.dataset
 
-    mlflow_dataset_params = {
-        "dataset_type": "synthetic",
-        "dataset_max_static_amplitude": d.parameters.amplitude_static_max,
-        "dataset_max_dynamic_amplitude": d.parameters.amplitude_dynamic_max,
-        "dataset_n_timeframes": d.parameters.T,
-        "dataset_freq_max": d.parameters.freq_max,
-        "dataset_l_max": d.parameters.l_max,
-        "dataset_resolution": d.parameters.mesh_resolution,
-        "dataset_complexity_c": (d.parameters.l_max + 1) ** 2,
-        "dataset_complexity_s": ((d.parameters.l_max + 1) ** 2) * d.parameters.freq_max,
-        "dataset_complexity": ((d.parameters.l_max + 1) ** 2) * (d.parameters.freq_max + 1),
-        "dataset_random_seed": d.parameters.random_seed,
-        "dataset_template": "icosphere",  # TODO: add this as parameter in the configuration
-        "dataset_center_around_mean": d.preprocessing.center_around_mean
-    }
+    # LOAD DATA
+    dm = get_datamodule(config)
+    model = get_lightning_module(config, dm)
+    trainer = get_lightning_trainer(trainer_args)
 
-    return mlflow_dataset_params
-
-
-def print_auto_logged_info(r):
-
-    tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
-    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
-    print("run_id: {}".format(r.info.run_id))
-    print("artifacts: {}".format(artifacts))
-    print("params: {}".format(r.data.params))
-    print("metrics: {}".format(r.data.metrics))
-    print("tags: {}".format(tags))
+    return dm, model, trainer
