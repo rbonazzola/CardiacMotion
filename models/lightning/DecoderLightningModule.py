f348965958f228ec745bbe5aa3c46cd882948445
rbonazzola <rodbonazzola@gmail.com>
2022-05-31 07:06:35 +0000

    Added generation figure to decoder PL module

--- models/lightning/DecoderLightningModule.py
+++ models/lightning/DecoderLightningModule.py
@@ -151,3 +151,89 @@ class TemporalDecoderLightning(pl.LightningModule):
     def test_epoch_end(self, outputs):
         loss_dict = self._average_over_batches(outputs, prefix="test_")
         self.log_dict(loss_dict, on_epoch=True, prog_bar=True, logger=True)
+
+
+    ########### PREDICTING
+    def predict_step(self, batch, batch_idx):
+
+        s_avg, s_t, z = self._unpack_data_from_batch(batch)
+        z = {"mu": z, "log_var": None}
+        s_avg_hat, shat_t = self(z)
+
+        ### IMAGES OF TEMPORAL AVERAGE
+        _s_avg = s_avg[0].cpu()
+        _s_avg_hat = s_avg_hat[0].cpu() + self.model.template_mesh.v
+        if self.params.dataset.preprocessing.center_around_mean:
+                _s_avg += self.model.template_mesh.v
+                _s_avg_hat += self.model.template_mesh.v
+        SyntheticMeshPopulation.render_mesh_as_png(_s_avg,
+                                                       self.model.template_mesh.f,
+                                                       f"temporal_avg_mesh_{batch_idx}_orig.png")
+        SyntheticMeshPopulation.render_mesh_as_png(_s_avg_hat.cpu() + self.model.template_mesh.v,
+                                                       self.model.template_mesh.f,
+                                                       f"temporal_avg_mesh_{batch_idx}_rec.png")
+
+        merge_pngs_horizontally(
+            f"temporal_avg_mesh_{batch_idx}_orig.png", 
+            f"temporal_avg_mesh_{batch_idx}_rec.png",
+            f"temporal_avg_mesh_{batch_idx}.png"
+        )
+
+        self.logger.experiment.log_artifact(
+            local_path=f"temporal_avg_mesh_{batch_idx}.png",
+            artifact_path="images", run_id=self.logger.run_id
+        )
+
+        ### ANIMATIONS OF MOVING MESH
+        if self.params.dataset.preprocessing.center_around_mean:
+            s_t = s_t.cpu() + self.model.template_mesh.v
+            s_hat_t = s_hat_t.cpu() + self.model.template_mesh.v
+            SyntheticMeshPopulation._generate_gif(s_t, self.model.template_mesh.f, f"moving_mesh_{batch_idx}_orig.gif")
+            SyntheticMeshPopulation._generate_gif(s_hat_t, self.model.template_mesh.f, f"moving_mesh_{batch_idx}_rec.gif")
+
+        merge_gifs_horizontally(
+            f"moving_mesh_{batch_idx}_orig.gif", 
+            f"moving_mesh_{batch_idx}_rec.gif",
+            f"moving_mesh_{batch_idx}.gif"
+        )
+
+        self.logger.experiment.log_artifact(
+            local_path=f"moving_mesh_{batch_idx}.gif",
+            artifact_path="animations", run_id=self.logger.run_id
+        )
+
+        return 0  # to prevent warning messages
+
+
+def merge_pngs_horizontally(png1, png2, output_png):
+    # https://www.tutorialspoint.com/python_pillow/Python_pillow_merging_images.htm
+    # Read the two images
+    image1 = Image.open(png1)
+    image2 = Image.open(png2)
+    # resize, first image
+    image1_size = image1.size
+    # image2_size = image2.size
+    new_image = Image.new('RGB', (2 * image1_size[0], image1_size[1]), (250, 250, 250))
+    new_image.paste(image1, (0, 0))
+    new_image.paste(image2, (image1_size[0], 0))
+    new_image.save(output_png, "PNG")
+
+
+def merge_gifs_horizontally(gif_file1, gif_file2, output_file):
+    # Create reader object for the gif
+    gif1 = imageio.get_reader(gif_file1)
+    gif2 = imageio.get_reader(gif_file2)
+
+    # Create writer object
+    new_gif = imageio.get_writer(output_file)
+
+    for frame_number in range(gif1.get_length()):
+        img1 = gif1.get_next_data()
+        img2 = gif2.get_next_data()
+        # here is the magic
+        new_image = np.hstack((img1, img2))
+        new_gif.append_data(new_image)
+
+    gif1.close()
+    gif2.close()
+    new_gif.close()
