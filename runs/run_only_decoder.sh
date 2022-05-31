1fcc2c7ab3c3842b9f1e09aaa79b7e1c09611b54
rbonazzola <rodbonazzola@gmail.com>
2022-05-31 09:46:47 +0000

    Changed bash script for decoder run

--- runs/run_only_decoder.sh
+++ runs/run_only_decoder.sh
@@ -16,16 +16,19 @@ python main.py \
   -c config_files/config_folded_c_and_s.yaml \
   --only_decoder \
   --n_channels_enc $N_CHANNELS \
+  --n_channels_dec_c $N_CHANNELS \
+  --n_channels_dec_s $N_CHANNELS \
   --w_kl 0 \
-  --batch_size 512 \
+  --batch_size 256 \
   --z_aggr_function "DFT" \
-  --learning_rate 0.00001 \
-  --dataset.N_subjects 320 \
-  --dataset.N_timeframes 4 \
+  --learning_rate 0.0001 \
+  --dataset.N_subjects 5120 \
+  --dataset.N_timeframes 50 \
+  --dataset.mesh_resolution 6 \
   --dataset.amplitude_static_max 0.2 \
   --dataset.amplitude_dynamic_max 0.1 \
   --dataset.freq_max 2 \
-  --dataset.l_max 1 \
+  --dataset.l_max 2 \
+  --gpus ${GPU_DEVICE:-0} \
   $@
-  # --gpus ${GPU_DEVICE:-0} \
 
