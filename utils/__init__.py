8a068671fe131a62d3d16d7b4e6b845062c12d64
rbonazzola <rodbonazzola@gmail.com>
2023-05-02 00:50:31 +0100

    Updated repo location

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,6 @@ import shlex
 import sys, os
 
 # add the repository's root directory to Python path 
-repo_root = check_output(shlex.split("git rev-parse --show-toplevel")).strip().decode('ascii')
+repo_root = f"{os.environ['HOME']}/Rodrigo_repos/CardiacMotion" #check_output(shlex.split("git rev-parse --show-toplevel")).strip().decode('ascii')
 sys.path.append(repo_root)
 sys.path.append(os.path.join(repo_root, "utils"))
