9852899405e26ea4bf69c8e36211a2e87dc720f0
rbonazzola <rodbonazzola@gmail.com>
2026-09-05 22:13:41 +0100

    Added center_around_mean option and pin_memory to cardiac data module

--- cardiac_motion/data/DataModules.py
+++ cardiac_motion/data/DataModules.py
@@ -1,4 +1,6 @@
 import os, sys
+import logging
+import time
 import numpy as np
 import re
 import glob
@@ -20,6 +22,8 @@ from cardio_mesh import (
 
 from cardio_mesh.procrustes import transform_mesh
 
+logger = logging.getLogger(__name__)
+
 def mse(s1, s2):
     return ((s1-s2)**2).sum(-1).mean(-1)
 
@@ -27,43 +31,69 @@ def mse(s1, s2):
 class CardiacMeshPopulationDataset(TensorDataset):
     
     def __init__(
-        self, 
-        root_path: str, 
+        self,
+        root_path: str,
         faces: Union[np.ndarray, str],
         N_subj: Union[int, None] = None,
         procrustes_transforms: Union[str, None] = None,
-        subsetting_matrix: Union[np.ndarray, None] = None,        
+        subsetting_matrix: Union[np.ndarray, None] = None,
         template_mesh = None,
         phases_filter = None,
-        static_shape: Literal["end_diastole", "temporal_mean", "end_systole"] = "end_diastole"
+        static_shape: Literal["end_diastole", "temporal_mean", "end_systole"] = "end_diastole",
+        center_around_mean: bool = False,
         ):
-        
+
         '''
           root_dir: root directory where all the mesh data is located.
           N_subj: if None, all the subjects are utilized.
           faces: F x 3 array (F is the number of faces)
           procrustes_transforms: Mapping from IDs to transforms ("rotation" and "traslation")
+          center_around_mean: if True, subtract the population mean shape (template_mesh.v)
+            from all meshes. Reduces data scale to small residuals, which stabilizes training.
         '''
-        
+
+        if center_around_mean and template_mesh is None:
+            raise ValueError("center_around_mean=True requires template_mesh to be provided.")
+
+        start = time.perf_counter()
         self._root_path = root_path
+        logger.info(
+            "Indexing cardiac meshes: root=%s, N_subj=%s, phases_filter=%s",
+            root_path,
+            N_subj,
+            phases_filter,
+        )
         self._paths = self._get_paths(N=N_subj, phases_filter=phases_filter)
-        
+
+        logger.info("Loading Procrustes transforms from %s", procrustes_transforms)
         self.procrustes_transforms = pkl.load(open(procrustes_transforms, "rb"))
-        
+        logger.info("Loaded %d Procrustes transforms", len(self.procrustes_transforms))
+
         self.ids = set(self._get_ids()).intersection(set(self.procrustes_transforms.keys()))
         self.ids = list(self.ids)
-        
+
         self._paths = { k: v for k, v in self._paths.items() if k in self.ids}
-        
+        self.ids = sorted(self._paths.keys())
+
         self.subsetting_matrix = subsetting_matrix
         self.faces = faces
         self.template_mesh = template_mesh
         self.static_shape = static_shape
+        self.center_around_mean = center_around_mean
+        n_frames = len(next(iter(self._paths.values()))) if self._paths else 0
+        logger.info(
+            "Cardiac dataset indexed: subjects=%d, frames_per_subject=%d, center_around_mean=%s, elapsed=%.2fs",
+            len(self.ids),
+            n_frames,
+            self.center_around_mean,
+            time.perf_counter() - start,
+        )
         
     
     def _get_paths(self, N=None, phases_filter=None):
             
         ids = sorted(os.listdir(self._root_path))
+        n_available_ids = len(ids)
         
         if N is not None:
             ids = ids[:N]
@@ -72,6 +102,8 @@ class CardiacMeshPopulationDataset(TensorDataset):
         
         dd = {}
         
+        logger.info("Scanning %d/%d subject folders for complete mesh time series", len(ids), n_available_ids)
+        skipped_incomplete = 0
         for id in ids:
             
             # paths = sorted(glob.glob(f"{self._root_path}/{id}/models/LV*.npy"))    
@@ -92,7 +124,14 @@ class CardiacMeshPopulationDataset(TensorDataset):
                         
             if ((phases_filter is None) and len(phases) == 50) or ((phases_filter is not None) and len(phases) == len(phases_filter)):
                 dd[id] = paths_filtered
+            else:
+                skipped_incomplete += 1
             
+        logger.info(
+            "Mesh scan complete: usable_subjects=%d, skipped_incomplete=%d",
+            len(dd),
+            skipped_incomplete,
+        )
         return dd            
        
         
@@ -113,7 +152,7 @@ class CardiacMeshPopulationDataset(TensorDataset):
             try:
                 s = np.load(p, allow_pickle=True)
             except ValueError as e:
-                
+                logger.warning("Could not load mesh file %s: %s", p, e)
                 continue
             
             if self.subsetting_matrix is not None:
@@ -129,21 +168,27 @@ class CardiacMeshPopulationDataset(TensorDataset):
         elif self.static_shape == "temporal_mean":
             s_t_avg = s_t.mean(axis=0)
         else:
-            raise NotImplemented
+            raise NotImplementedError
         
-        dev_from_tmp_avg = np.array([ mse(s_t[j], s_t_avg) for j, _ in enumerate(s_t) ])
-        dev_from_tmp_avg = Tensor(dev_from_tmp_avg)
-        
+        dev_from_tmp_avg = mse(s_t, s_t_avg.unsqueeze(0))
+
         if self.template_mesh is not None:
-            dev_from_sphere = np.array([ mse(s_t[j], self.template_mesh.v) for j, _ in enumerate(s_t) ])
-            dev_from_sphere = Tensor(dev_from_sphere)
+            template_v = Tensor(self.template_mesh.v).unsqueeze(0)
+            dev_from_sphere = mse(s_t, template_v)
         else:
             dev_from_sphere = None
-        
+
+        if self.center_around_mean:
+            # Shift meshes to residuals w.r.t. population mean.
+            # d_content and d_style are invariant to this shift so they stay as-is.
+            mean_v = Tensor(self.template_mesh.v)
+            s_t     = s_t     - mean_v
+            s_t_avg = s_t_avg - mean_v
+
         dd = {
           "s_t": s_t,
           "time_avg_s": s_t_avg,
-          "d_content": dev_from_tmp_avg,  
+          "d_content": dev_from_tmp_avg,
           "d_style": dev_from_sphere
         }
         
@@ -182,6 +227,13 @@ class CardiacMeshPopulationDM(pl.LightningDataModule):
         self.batch_size = batch_size
         self.split_lengths = self._get_split_lengths(split_lengths)
         self.num_workers=num_workers
+        logger.info(
+            "CardiacMeshPopulationDM initialized: dataset=%d, batch_size=%d, split_lengths=%s, num_workers=%d",
+            len(self.dataset),
+            self.batch_size,
+            self.split_lengths,
+            self.num_workers,
+        )
         
         
     def _get_split_lengths(self, split_lengths):
@@ -192,23 +244,22 @@ class CardiacMeshPopulationDM(pl.LightningDataModule):
             train_len = int(0.6 * len(self.dataset))
             test_len = int(0.2 * len(self.dataset))
             val_len = len(self.dataset) - train_len - test_len
-            self.split_lengths = [train_len, val_len, test_len]
         
-        elif all([l >= 1 for l in _split_lengths]):                        
+        elif all([l >= 1 for l in _split_lengths]):
             try:
                 train_len = _split_lengths[0]
-                test_len = _split_lengths[1]
-                val_len = _split_lengths[2]
+                val_len   = _split_lengths[1]
+                test_len  = _split_lengths[2]
             except IndexError:
                 raise IndexError(f"split_lengths should have length three, instead is {_split_lengths}")
 
         elif all([l < 1 for l in _split_lengths]):
             train_len = int(_split_lengths[0] * len(self.dataset))
-            test_len = int(_split_lengths[1] * len(self.dataset))
+            val_len   = int(_split_lengths[1] * len(self.dataset))
             if len(_split_lengths) == 2:
-                val_len = len(self.dataset) - train_len - test_len
-            elif len(_split_lengths) == 3:            
-                val_len = int(_split_lengths[2] * len(self.dataset))
+                test_len = len(self.dataset) - train_len - val_len
+            elif len(_split_lengths) == 3:
+                test_len = int(_split_lengths[2] * len(self.dataset))
             else:
                 raise ValueError("Bad values for split lengths. Expecting 2 or 3 fractions/integers.")
                 
@@ -224,7 +275,16 @@ class CardiacMeshPopulationDM(pl.LightningDataModule):
         #     cardiac_population=self.cardiac_population
         # )
         
+        start = time.perf_counter()
+        logger.info("Splitting dataset: lengths=%s", self.split_lengths)
         self.train_dataset, self.val_dataset, self.test_dataset = random_split(self.dataset, self.split_lengths)
+        logger.info(
+            "Dataset split complete: train=%d, val=%d, test=%d, elapsed=%.2fs",
+            len(self.train_dataset),
+            len(self.val_dataset),
+            len(self.test_dataset),
+            time.perf_counter() - start,
+        )
         
         # indices = list(range(sum(self.split_lengths)))
 
@@ -233,16 +293,16 @@ class CardiacMeshPopulationDM(pl.LightningDataModule):
         # self.test_indices = indices[self.split_lengths[0]+self.split_lengths[1]:self.split_lengths[0]+self.split_lengths[1]+self.split_lengths[2]]        
  
     def train_dataloader(self):
-        # return DataLoader(self.train_dataset, sampler=SubsetRandomSampler(self.train_indices), batch_size=self.batch_size, num_workers=self.num_workers)
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+        logger.info("Creating train dataloader: batch_size=%d, num_workers=%d", self.batch_size, self.num_workers)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=torch.cuda.is_available())
 
     def val_dataloader(self):
-        # return DataLoader(self.train_dataset, sampler=SubsetRandomSampler(self.val_indices), batch_size=self.batch_size, num_workers=self.num_workers)
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+        logger.info("Creating val dataloader: batch_size=%d, num_workers=%d", self.batch_size, self.num_workers)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=torch.cuda.is_available())
 
     def test_dataloader(self):
-        # return DataLoader(self.train_dataset, sampler=SubsetRandomSampler(self.test_indices), batch_size=self.batch_size, num_workers=self.num_workers)
-        return DataLoader(self.test_dataset, batch_size=1, num_workers=self.num_workers)
+        logger.info("Creating test dataloader: batch_size=%d, num_workers=%d", self.batch_size, self.num_workers)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=torch.cuda.is_available())
 
 
 class GenericDataModule(pl.LightningDataModule):
@@ -331,6 +391,7 @@ class GenericDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=self.shuffle_train,
             num_workers=self.num_workers,
+            pin_memory=torch.cuda.is_available(),
         )
 
     def val_dataloader(self) -> DataLoader:
@@ -342,6 +403,7 @@ class GenericDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=self.shuffle_val,
             num_workers=self.num_workers,
+            pin_memory=torch.cuda.is_available(),
         )
 
     def test_dataloader(self) -> DataLoader:
@@ -353,4 +415,5 @@ class GenericDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=self.shuffle_test,
             num_workers=self.num_workers,
+            pin_memory=torch.cuda.is_available(),
         )
